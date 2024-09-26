7b0d2fe74d6169e234bc80c9b21929213edfcdc8
Majo <olaecheamariajose@gmail.com>
2024-09-26 11:11:18 -0300

    Feat. SimpleForm + creacion de rutas de navegacion

---
+++ 03-hook-react/src/02-useEffect/SimpleForm.jsx
@@ -0,0 +1,12 @@
+const SimpleForm = () => {
+  return (
+    <div>
+      <h1>Formulario Simple</h1>
+
+      <hr />
+      <input type="text" placeholder="Username" name="username" />
+    </div>
+  );
+};
+
+export default SimpleForm;
--- 03-hook-react/src/main.jsx
+++ 03-hook-react/src/main.jsx
@@ -1,14 +1,39 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import "./index.css";
 import HookApp from "./HookApp";
 import CounterApp from "./01-useState/CounterApp";
 import CounterWithCustomHook from "./01-useState/CounterWithCustomHook";
+import SimpleForm from "./02-useEffect/SimpleForm";
+
+const Navigation = () => {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link to="/counter">Counter App</Link>
+        </li>
+        <li>
+          <Link to="/custom-counter">Counter with Custom Hook</Link>
+        </li>
+        <li>
+          <Link to="/simple-form">Simple Form - useEffect</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <HookApp />
-    <CounterApp />
-    <CounterWithCustomHook />
+    <Router>
+      <Navigation />
+      <Routes>
+        <Route path="/counter" element={<CounterApp />} />
+        <Route path="/custom-counter" element={<CounterWithCustomHook />} />
+        <Route path="/simple-form" element={<SimpleForm />} />
+      </Routes>
+    </Router>
   </StrictMode>
 );
