7e52ba2ce899ba76ffe40c33a33d5be8ee0b774f
Majo <olaecheamariajose@gmail.com>
2024-09-26 09:25:05 -0300

    Edit CounterApp

--- 02-react/src/CounterApp.jsx
+++ 02-react/src/CounterApp.jsx
@@ -3,14 +3,24 @@ import PropTypes from "prop-types";
 const CounterApp = ({ value }) => {
   const [counter, setCounter] = useState(value);
 
-  const handleAdd = (event) => {
+  const handleAdd = () => {
     setCounter(counter + 1);
   };
+
+  const handleSustract = () => {
+    setCounter(counter - 1);
+  };
+  const handleReset = () => {
+    setCounter(value);
+  };
+
   return (
     <div>
       <h1>CounterApp</h1>
       <h2>{counter}</h2>
       <button onClick={handleAdd}>+1</button>
+      <button onClick={handleSustract}>-1</button>
+      <button onClick={handleReset}>Reset</button>
     </div>
   );
 };
